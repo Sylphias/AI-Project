207ffc7b87fd0ac539624fa7ae6da0643b23314e
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2018-07-10 08:21:07 +0000

    Removed commented code

--- pytorch_vqa/resnet.py
+++ pytorch_vqa/resnet.py
@@ -80,6 +80,5 @@ def resnet152(pretrained=False):
     """
     model = ResNet(Bottleneck, [3, 8, 36, 3])
     if pretrained:
-        # model.load_state_dict(model_zoo.load_url(model_urls['resnet152']))
         model.load_state_dict(torch.load('data/resnet152-caffe.pth'))
     return model
